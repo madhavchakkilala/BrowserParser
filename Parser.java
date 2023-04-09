4ca35a5aa5252f2b9bdb85008ff49d7612609503
madhavchakkilala <57663584+madhavchakkilala@users.noreply.github.com>
2023-04-10 01:23:57 +0530

    dfs logic base setup

--- Parser.java
+++ Parser.java
@@ -1,6 +1,3 @@
-import java.util.Scanner;
-import java.util.Stack;
-
 public class Parser {
 
     private static Parser instance = new Parser();
@@ -10,46 +7,50 @@ public class Parser {
     }
 
     public DOMElement parse(String input){
-        DOMElement rootElement = new DOMElement();
-        dfs(rootElement, input);
+        DOMElement rootElement = new DOMElement("SUPER_ROOT");
+        Iterator iterator = new Iterator();
+        dfs(rootElement, input, iterator);
         return rootElement;
     }
 
-    public void dfs(DOMElement element, String input){ //<div>hi<
-                                                        //div>
-            String currentInput = "";
-            for(int i=0;i<input.length();i++) {
-                if(input.charAt(i) == '<') {
-
-                }
+    public void dfs(DOMElement element, String input, Iterator iterator){
+        DOMElement currentInput = getNextElement(input, iterator);
+        while (currentInput != null){
+            switch(currentInput.elementType){
+                case "OPENING_ELEMENT":
+                    if(currentInput == null){
+                        return;
+                    }
+                    element.elements.add(currentInput);
+                    dfs(currentInput, input, iterator);
+                    currentInput = getNextElement(input, iterator);
+                    break;
+                case "CLOSING_ELEMENT":
+                    return;
+                case "LEAF_ELEMENT":
+                    element.elements.add(currentInput);
+                    currentInput = getNextElement(input, iterator);
+                    break;
             }
-            //find the next tag
-            //if opening tag:
-            //    create a child and add it to the list and call dfs(newlyCreatedNode, input);
-            //else if text:
-            //    add a leaf element
-            //else if closing tag:
-            //    do nothing
-    } //    >
+        }
+    }
 
-    public DOMElement getNextElement(String input, int index){
+    public DOMElement getNextElement(String input, Iterator iterator){
         boolean isOpeningBraceFound = false;
         String currentString = "";
-        for(; index < input.length();index++){
-            if(currentString.charAt(index) == '<'){
+        for(; iterator.index < input.length();iterator.index++){
+            if(currentString.charAt(iterator.index) == '<'){
                 isOpeningBraceFound = true;
-            } else if(currentString.charAt(index) == '>') {
+            } else if(currentString.charAt(iterator.index) == '>') {
                 return new DOMElement(currentString);
             } else {
-                currentString += currentString.charAt(index);
+                currentString += currentString.charAt(iterator.index);
             }
         }
         return null;
     }
 
-
-
-
-
-
+    class Iterator{
+        int index = 0;
+    }
 }
